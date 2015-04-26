90c015e28a7cab0d4d97d034630bd45c9d014797
Jan Machacek <jan.machacek@gmail.com>
2015-04-26 16:55:59 +0100

    Scaffolding over WS

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 var express = require('express');
-var concat = require('concat-stream');
 var app = express();
+var expressWs = require('express-ws')(app)
 
-app.use(function(req, res, next){
-  req.pipe(concat(function(data){
+app.use(express.static(__dirname + '/public'))
+
+var concat = require('concat-stream');
+app.use(function(req, res, next) {
+  req.pipe(concat(function(data) {
     req.body = data;
     next();
   }));
 });
 
+app.ws('/exercise', function(ws, req) {
+});
+var aWss = expressWs.getWss('/exercise');
 app.post('/exercise/:userId/:sessionId', function(req, res) {
   console.log(req.body.toString('utf-8'));
   res.type('application/json');
   res.send('{}');
+  aWss.clients.forEach(function (client) {
+    client.send(req.body.toString('utf-8'));
+  });
 });
 
 app.listen(process.env.PORT || 8080);
